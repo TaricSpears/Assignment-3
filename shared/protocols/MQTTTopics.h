705a5653c4e626e6673bdc4d2f37b74e04e101b3
Davide Bartoli <davide30.bartoli@gmail.com>
2025-06-13 16:41:13 +0200

    doc folder

--- shared/protocols/MQTTTopics.h
+++
@@ -1,7 +0,0 @@
-namespace ClimateTopics
-{
-    constexpr auto TEMPERATURE = "sensors/temperature";
-    constexpr auto FREQUENCY = "control/frequency";
-    constexpr auto WINDOW_STATUS = "window/status";
-    constexpr auto ALARM = "system/alarm";
-}
